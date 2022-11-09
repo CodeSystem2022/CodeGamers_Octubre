d90a5e20c20f1772f32a9f4a07cd0535338a2a43
Diego Di Stefano <92904868+diegodistefano@users.noreply.github.com>
2022-11-08 21:51:01 -0300

    Update Ejercicios_Octubre.py

--- Ejercicios_Octubre.py
+++ Ejercicios_Octubre.py
@@ -1,1 +1,21 @@
 # Ejercicios de Octubre en Python
+
+
+
+# Di Stefano Diego
+
+class Cubo:
+     def __init__(self,ancho, alto, profundidad):
+          self.ancho = ancho
+          self.alto = alto
+          self.profundidad = profundidad
+
+     def calcular_volumen(self):
+        return self.ancho * self.alto * self.profundidad
+      
+ancho = int(input('Ancho: '))
+alto = int(input('Alto: '))
+profundidad = int(input('Profundidad: '))
+
+cubo1 = Cubo(ancho,alto,profundidad)
+print(f'El volumen es: {cubo1.calcular_volumen()}')

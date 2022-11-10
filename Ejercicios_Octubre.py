188aa0560f2cd72c1fca0bd387916f6f3861d352
distefanorobertoo <112036308+distefanorobertoo@users.noreply.github.com>
2022-11-09 22:25:38 -0300

    Update Ejercicios_Octubre.py

--- Ejercicios_Octubre.py
+++ Ejercicios_Octubre.py
@@ -68,3 +68,28 @@ profundidad = int(input('Profundidad: '))
 
 cubo1 = Cubo(ancho,alto,profundidad)
 print(f'El volumen es: {cubo1.calcular_volumen()}')
+
+
+
+
+
+# Roberto Di Stefano
+class Rectangulo:
+    """
+    Creamos una clase llamada Rectangulo, debe tenér 2 atributos: altura y base
+    el nombre del método sera calcular area utilizando la formula:
+    area = base * altura. Pero la base y la altura deben ser ingresadas por
+    el usuario y los objetos deben ser tres.
+    """
+
+    def __init__(self, altura, base):
+        self.altura = altura
+        self.base = base
+
+    def calcular_area(self):
+        return self.base * self.altura
+
+base = int(input('Digite el número para la base del rectangulo: '))
+altura = int(input('Digite el número para la altura del rectangulo: '))
+rectangulo1 = Rectangulo(base, altura)
+print(f'El area del rectangulo es: {rectangulo1.calcular_area()}')

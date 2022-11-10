7d1c417a5667bcf7e8fda79f5214d8c2e7c2e06c
gasexgg <111871436+gasexgg@users.noreply.github.com>
2022-11-09 23:15:20 -0300

    Update Ejercicios_Octubre.java

--- Ejercicios_Octubre.java
+++ Ejercicios_Octubre.java
@@ -72,3 +72,30 @@ public class Ciclos11 {
     JOptionPane.showMessageDialog(null, "El producto de los números impares es: "+producto);
     }
 }
+
+
+/*
+Gastón Exequiel Garay
+
+ Ejercicio 12: Pedir un número y calcular su factorial
+Hacerlo con las dos clases, Scanner y JOptionPane
+ */
+package ciclos12;
+
+//import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+
+public class Ciclos12 {
+    public static void main(String[] args) {
+        //Scanner entrada = new Scanner(System.in);
+        long factorial = 1;
+        //System.out.println("Digite un número");
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite un número: "));
+        for(int i=1; i<=numero; i++){
+            factorial *= i;
+        }
+        //System.out.println("\nEl factorial del número ingresado es: "+factorial);
+        JOptionPane.showMessageDialog(null, "el factorial del número ingresado es: "+factorial);
+    }
+}
